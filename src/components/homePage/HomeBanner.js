20fa01fd1bf3916a67bb84646590ed9d74da8491
Sam S <Saprykinse@gmail.com>
2020-09-26 11:16:26 +0300

    initial commit

---
+++ src/components/homePage/HomeBanner.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from "react"
+//Custom Hook
+import useWindowSize from "../../hooks/useWindowSize"
+//Context
+import { useGlobalStateContext } from "../../context/globalContext"
+//Styled Components
+import {
+  Banner,
+  Video,
+  BannerTitle,
+  Canvas,
+  Headline,
+} from "../../styles/homeStyles"
+
+const HomeBanner = ({ onCursor }) => {
+
+  
+
+  const container = {
+    initial: { y: 800 },
+    animate: {
+      y: 0,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  }
+  const item = {
+    initial: { y: 800 },
+    animate: {
+      y: 0,
+      transition: {
+        duration: 1,
+        ease: [0.6, 0.05, -0.01, 0.9],
+      },
+    },
+  }
+
+  return (
+    <Banner>
+      <Video>
+        <video
+          height="100%"
+          width="100%"
+          loop
+          autoPlay
+          muted
+          src={require("../../assets/video/video.mp4")}
+        />
+      </Video>
+      <BannerTitle variants={container} initial="initial" animate="animate">
+        <Headline variants={item}>DIG</Headline>
+        <Headline variants={item}>DEEP</Headline>
+      </BannerTitle>
+    </Banner>
+  )
+}
+
+export default HomeBanner
